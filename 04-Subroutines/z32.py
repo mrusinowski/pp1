613f6c386e31c97fba7072f795aac4a10e08ba43
Michał <michalr101@gmail.com>
2019-11-06 13:50:35 +0100

    p

--- 04-Subroutines/z32.py
+++ 04-Subroutines/z32.py
@@ -12,8 +12,8 @@ m = [[1, 2, 0],[0, 0, 3],[5, 1, 1]]
 print('Macierz:')
 for w in range(3):
     for k in range(3):
-            if k==0 and w>0:
-                print(f'\n{m[w][k]}', end=' ')
-            else: print(m[w][k], end=' ')
+        if k==0 and w>0:
+            print(f'\n{m[w][k]}', end=' ')
+        else: print(m[w][k], end=' ')
 print('\n\nMacierz transponowana:')
 transpozycja(m)
